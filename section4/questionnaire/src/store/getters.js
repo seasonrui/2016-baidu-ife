a2f61f12591425a4ab2c2daebcb0872009f59b43
season <season@seasondeMacBook-Pro.local>
2017-02-26 17:49:14 +0800

    add tasks

--- section4/questionnaire/src/store/getters.js
+++
@@ -1,9 +0,0 @@
-export const list = state => {
-  return state.list;
-};
-export const editing = state => {
-  return state.editing;
-};
-export const status = state => {
-  return state.status;
-};
